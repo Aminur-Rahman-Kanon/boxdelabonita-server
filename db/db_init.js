092a654e462a2ea86a5c9bc204c1b875e0209d6d
Aminur-Rahman-Kanon <kanon754@gmail.com>
2023-11-14 04:38:51 +0000

    database edit 1

--- db/db_init.js
+++ db/db_init.js
@@ -5,7 +5,9 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOSTNAME}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool = new Pool({
-    connectionString: isProduction ? process.env.PG_DATABASE_URL : process.env.PG_EXT_DATABASE,
+    connectionString: isProduction ? process.env.PG_DATABASE_URL : connectionString,
+    idleTimeoutMillis: 0,
+    connectionTimeoutMillis: 0,
     ssl: {
         rejectUnauthorized: false,
     },
